4ae68c81eea90d91d20207b94855dea5d407c22d
Bazukaaa <leekotic25@gmail.com>
2021-08-07 14:26:55 +0700

    Update unittest

--- subject-reservation-web-demo/tests/unit/example.spec.js
+++ subject-reservation-web-demo/tests/unit/example.spec.js
@@ -6,7 +6,9 @@ describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+      propsData: { 
+        msg 
+      }
     })
     expect(wrapper.text()).to.include(msg)
   })
